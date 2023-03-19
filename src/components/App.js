965af287907978ab54960c22a731e469327ff37f
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-19 15:45:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
@@ -7,39 +7,37 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [sortAscending, setSortAscending] = useState(true);  
 
-  const fetchData = async () => {
+  const handleFetch = async () => {
     setIsLoading(true);
-    const response = await fetch('https://content.newtonschool.co/v1/pr/main/users');
-    const data = await response.json();
-    setIsLoading(false);
-    setUsers(data);
-  };
-  
-  const handleSort = () => {
-  const sortedUsers = [...users].sort((a,b) => {
-    is(sortAscending){
-      return a.name.length - b.name,length;
-    } else {
-      return b.name.length - a.name.length;
+    try {
+      const response = await fetch('https://content.newtonschool.co/v1/pr/main/users');
+      const data = await response.json();
+      setUsers(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
     }
-        });
-    setUsers(sortedUsers);
+  }
+
+  const handleSort = () => {
     setSortAscending(!sortAscending);
-  };
+    if (sortAscending) {
+      setUsers([...users].sort((a, b) => a.name.length - b.name.length));
+    } else {
+      setUsers([...users].sort((a, b) => b.name.length - a.name.length));
+    }
+  }
+
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn" onClick={fetchData}>Fetch User Data</button>
+      <button className="fetch-data-btn" onClick={handleFetch}>Fetch User Data</button>
       <button className="sort-btn" onClick={handleSort}>
-        {sortAscending ?
-        "Sort by name length (ascending)" :
-        "Sort by name length (descending)" }
+        {sortAscending ? "Sort by name length (ascending)" : "Sort by name length (descending)"}
       </button>
-{isLoading && 
-      <p>Loading...</p>}
- {!isLoading && (
+      {isLoading ? <p>Loading...</p> :
       <div className='users-section'>
-   {users.map(user => (
+        {users.map((user) => (
           <li key={user.id}>
             <section className='id-section'>{user.id}</section>
             <section className='name-email-section'>
@@ -47,12 +45,10 @@ const App = () => {
               <p className='email'>Email: {user.email}</p>
             </section>
           </li>
- ))}
-      </div>
-)}
+        ))}
+      </div>}
     </div>
   )
 }
 
-
-export default App;
+export default App;
